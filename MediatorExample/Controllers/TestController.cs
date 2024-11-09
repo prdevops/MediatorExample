c6f0c24676ce7b7c4a7a17d434c06fe5639b2b31
mushfig-agahyev-developer <mushfig.aghayev.webdeveloper@gmail.com>
2024-11-09 22:24:50 +0400

    update

---
+++ MediatorExample/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MediatorExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/<TestController>
+        [HttpGet]
+        public string Get()
+        {
+            return "I am update";
+        }
+
+        // GET api/<TestController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "I am update";
+        }
+    }
+}
